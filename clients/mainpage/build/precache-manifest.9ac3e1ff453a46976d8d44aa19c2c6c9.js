b9dddd3eb0e597c4e956070b8fec46199dac4434
Haiyang Hong <hong961127@gmail.com>
2020-08-11 16:23:35 -0700

    Finish 8-11

---
+++ clients/mainpage/build/precache-manifest.9ac3e1ff453a46976d8d44aa19c2c6c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322db455dd83a9bff7d48f0bbb4c6098",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e471802a87c38e947496",
+    "url": "/static/css/main.51228835.chunk.css"
+  },
+  {
+    "revision": "a1692fb9ef17bd323a9a",
+    "url": "/static/js/2.94391ff4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94391ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e471802a87c38e947496",
+    "url": "/static/js/main.7e4fdaad.chunk.js"
+  },
+  {
+    "revision": "609c3773dd169effc028",
+    "url": "/static/js/runtime-main.9768d8fd.js"
+  }
+]);
---
+++ clients/orgchart/build/precache-manifest.b424f33d45a9acb98e191e22fa8f1d4f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94ede0050eeccbff492e001a431b32a",
+    "url": "/orgchart/index.html"
+  },
+  {
+    "revision": "013c2c721d5b77200efb",
+    "url": "/orgchart/static/css/2.72937474.chunk.css"
+  },
+  {
+    "revision": "a86b4a037c702b6b5374",
+    "url": "/orgchart/static/css/main.95047270.chunk.css"
+  },
+  {
+    "revision": "013c2c721d5b77200efb",
+    "url": "/orgchart/static/js/2.37a5fe6f.chunk.js"
+  },
+  {
+    "revision": "ecbe6d6404ba762e6624ccaad7776db1",
+    "url": "/orgchart/static/js/2.37a5fe6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86b4a037c702b6b5374",
+    "url": "/orgchart/static/js/main.d2b13f8b.chunk.js"
+  },
+  {
+    "revision": "9b3f7ef634accf461408",
+    "url": "/orgchart/static/js/runtime-main.e102dfed.js"
+  },
+  {
+    "revision": "82c00904dec3690f60f2793dd24d380e",
+    "url": "/orgchart/static/media/csv-empty.82c00904.png"
+  },
+  {
+    "revision": "85b693482a158cf9b50342b3748b4d1b",
+    "url": "/orgchart/static/media/csv.85b69348.png"
+  },
+  {
+    "revision": "71de781d533079d8b25960026d96120f",
+    "url": "/orgchart/static/media/feature1.71de781d.svg"
+  },
+  {
+    "revision": "51caed0e97df7e240625bdfbf499967b",
+    "url": "/orgchart/static/media/feature2.51caed0e.svg"
+  },
+  {
+    "revision": "84265dc2fdbe782d3340d2652c9bfdfa",
+    "url": "/orgchart/static/media/feature3.84265dc2.svg"
+  },
+  {
+    "revision": "00726d463b930526ccabc7486040cf66",
+    "url": "/orgchart/static/media/feature4.00726d46.svg"
+  },
+  {
+    "revision": "5558a031bcce64e154c4caeeb52aad88",
+    "url": "/orgchart/static/media/feature5.5558a031.svg"
+  },
+  {
+    "revision": "d954b43e264157e570f8d0ff5257e836",
+    "url": "/orgchart/static/media/finishline.d954b43e.svg"
+  },
+  {
+    "revision": "1f2673cf2b8360696d5264fc68387109",
+    "url": "/orgchart/static/media/jumbo.1f2673cf.svg"
+  },
+  {
+    "revision": "8b4b6342e095a990b390f7d66fa051df",
+    "url": "/orgchart/static/media/orgchart-demo.8b4b6342.png"
+  }
+]);

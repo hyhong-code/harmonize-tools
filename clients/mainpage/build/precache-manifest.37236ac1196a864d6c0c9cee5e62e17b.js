9d90c5ed958655325b979fe5c5ee87ca2d48bb71
Haiyang Hong <hong961127@gmail.com>
2020-08-12 16:39:09 -0700

    trim files

---
+++ clients/mainpage/build/precache-manifest.37236ac1196a864d6c0c9cee5e62e17b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8526c911a7544dca1c737e3bf9968f4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "accc76c75f2a0776c5fe",
+    "url": "/static/css/main.51228835.chunk.css"
+  },
+  {
+    "revision": "a1692fb9ef17bd323a9a",
+    "url": "/static/js/2.94391ff4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.94391ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "accc76c75f2a0776c5fe",
+    "url": "/static/js/main.d3f1923e.chunk.js"
+  },
+  {
+    "revision": "609c3773dd169effc028",
+    "url": "/static/js/runtime-main.9768d8fd.js"
+  }
+]);
